7d5523f0ec6d9761b5bd6388c9914b5216bb4cb0
Bardouni Naoufal <bardouni.naoufal@gmail.com>
2022-10-20 16:12:27 +0100

    Create s2.ts

---
+++ s2.ts
@@ -0,0 +1,73 @@
+export type A = {
+	id: string
+	acc?
+}
+
+export type Q = string|null;
+
+export default class {
+
+	// static Mutation = {
+	// 	async updateUser(name: string, id: string){
+	// 		return {
+	// 			id,
+	// 			name
+	// 		}
+	// 	}
+	// }
+
+	static Query = {
+		// user(_, {id, acc}: {id: string, acc?}, ctx){
+		// user(_, tp: A, ctx){
+		// 	return {
+		// 		id,
+		// 		name: "userName" as string
+		// 	}
+		// },
+
+		// multiReturnWithExplicit(_, tp, ctx){
+		// 	if(true){
+		// 		return null;
+		// 	}
+		// 	return "hello" as string;
+		// },
+		// multiReturnWithoutExplicit(_, tp, ctx){
+		// 	if(true){
+		// 		return null;
+		// 	}
+		// 	return "hello";
+		// },
+
+		// explicitStringOrNull(_, tp, ctx){
+		// 	return "hello" as string|null
+		// },
+		// explicitStringOrUndefined(_, tp, ctx){
+		// 	return "hello" as string|undefined;
+		// },
+		string(_, tp, ctx){
+			return "hello";
+		},
+		explicitString(_, tp, ctx){
+			return "hello" as string;
+		},
+
+		null(_, tp, ctx){
+			return null;
+		},
+		undefined(_, tp, ctx){
+			return undefined;
+		},
+		void(_, tp, ctx){},
+		emptyReturn(){return},
+		// nullOrUndefined(){
+		// 	return null as null|undefined;
+		// }
+
+		// typeOrNull(_, tp, ctx){
+		// 	let a : Q = null;
+		// 	return a;
+		// }
+	}
+
+
+}
